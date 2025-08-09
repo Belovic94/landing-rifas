2dc275bc24b77b4cdffdb525c88b263c65ed4209
Belovic94 <victor.belosevich94@gmail.com>
2025-08-09 16:17:04 -0300

    Fix webhook notification

--- server/index.js
+++ server/index.js
@@ -1,25 +1,22 @@
 import express from 'express';
 import path from 'path';
 import bodyParser from 'body-parser';
-import mercadopago from 'mercadopago';
-import { fileURLToPath } from 'url';
+import {MercadoPagoConfig, Payment, Preference} from 'mercadopago';
 import dotenv from 'dotenv';
 
 dotenv.config({ path: '.env.local' }); 
 
-
-
 const app = express();
 app.use(bodyParser.json());
 
-/* const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-app.use(express.static(path.join(__dirname, 'public'))); */
+const client = new MercadoPagoConfig({ accessToken: process.env.ACCESS_TOKEN });
+const preference = new Preference(client);
 
-const client = new mercadopago.MercadoPagoConfig({ accessToken: process.env.ACCESS_TOKEN });
-const preference = new mercadopago.Preference(client);
+const RIFA_PRICE = 10;
 
-const rifa_price = 10;
+const calcularRifas = (amount) => {
+	return amount / RIFA_PRICE;
+}
 
 app.post('/create-preference', async (req, res) => {
 
@@ -32,47 +29,51 @@ app.post('/create-preference', async (req, res) => {
 	const aPreference = {
 		body: {
 			items: [{
-					title: `Compra de ${amount} rifas`,
-					quantity: Number(amount),
-					unit_price: parseFloat(rifa_price),
+				title: `Compra de ${amount} rifas`,
+				quantity: Number(amount),
+				unit_price: parseFloat(RIFA_PRICE),
 			}]
 		}
 	}
 
 	try {
-			const response = await preference.create(aPreference);
-			console.log("preference_id: ", response.init_point);
-			res.json({ init_point: response.init_point });
+		const response = await preference.create(aPreference);
+		console.log("preference_id: ", response.init_point);
+		res.json({ init_point: response.init_point });
 	} catch (err) {
-			console.error('Error creando preferencia', err);
-			res.status(500).send('Error');
+		console.error('Error creando preferencia', err);
+		res.status(500).send('Error');
 	}
 });
 
 app.post('/webhook', async (req, res) => {
-  const { type, data } = req.body;
+  	const { type, data } = req.body;
 
 	if (type === 'payment') {
-    try {
-      const payment = await mercadopago.payment.findById(data.id);
-			const info = payment.body;
+		try {
+			const paymentData = await new Payment(client).get({id: data.id});
+			const info = paymentData.body;
 
-      if (info.status === 'approved') {
-        const email = info.payer.email;
-        const monto = info.transaction_amount;
-        const rifas = calcularRifas(monto);
+			if (info.status === 'approved') {
+				const email = info.payer.email;
+				const amount = info.transaction_amount;
+				const rifas = calcularRifas(amount);
 
-        usuarios[info.id] = { email, rifas };
+				//usuarios[info.id] = { email, rifas }; // HAY que agregar a la base.
 
-        console.log(`✅ Pago aprobado de ${email} por $${monto}`);
-        console.log(`🎟️ Rifas asignadas: ${rifas}`);
-      }
-    } catch (err) {
-      console.error('Error consultando pago:', err);
-    }
-  }
+				console.log(`✅ Pago aprobado de ${email} por $${amount}`);
+				console.log(`🎟️ Rifas asignadas: ${rifas}`);
+			}
+			res.sendStatus(200);
+			return;
+		} catch (err) {
+			console.error('Error consultando pago:', err);
+			res.sendStatus(err.status);
+			return;
+		}
+	}
+	res.sendStatus(404);
 
-  res.sendStatus(200);
 });
 
 app.get('/', (req, res) => {
