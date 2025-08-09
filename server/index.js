8dc3c848317c3ce501726880ee9d2384d0e6eae6
Belovic94 <victor.belosevich94@gmail.com>
2025-08-09 16:58:14 -0300

    Add uuid

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import path from 'path';
 import bodyParser from 'body-parser';
 import {MercadoPagoConfig, Payment, Preference} from 'mercadopago';
 import dotenv from 'dotenv';
+import { v4 as uuidv4 } from 'uuid';
 
 dotenv.config({ path: '.env.local' }); 
 
@@ -23,22 +24,36 @@ app.post('/create-preference', async (req, res) => {
 	const { amount } = req.body;
 
 	if (!amount || isNaN(amount)) {
-    return res.status(400).json({ error: 'amount inválido o faltante' });
-  }
+    	return res.status(400).json({ error: 'amount inválido o faltante' });
+  	}
+	
+	const externalRef = uuidv4();
+
+	// Generar un campo en la base que tenga externalRef 
+	// Los numeros de las rifas
+	// Poner el status
+	/* {
+		id: externalRef,
+		numbers:  [list de numeros]
+		status: Pending, Paid
+	}*/
 
 	const aPreference = {
 		body: {
 			items: [{
+				id: externalRef,
+				category_id: "Rifas",
 				title: `Compra de ${amount} rifas`,
 				quantity: Number(amount),
 				unit_price: parseFloat(RIFA_PRICE),
-			}]
+			}],
+			external_reference: externalRef
 		}
 	}
 
 	try {
 		const response = await preference.create(aPreference);
-		console.log("preference_id: ", response.init_point);
+		console.log(`Create preference with init_point: ${response.init_point} and external_reference: ${externalRef}`);
 		res.json({ init_point: response.init_point });
 	} catch (err) {
 		console.error('Error creando preferencia', err);
@@ -51,15 +66,16 @@ app.post('/webhook', async (req, res) => {
 
 	if (type === 'payment') {
 		try {
-			const paymentData = await new Payment(client).get({id: data.id});
-			const info = paymentData.body;
+			const payment = await new Payment(client).get({id: data.id});
+			console.log(`Arrive payment with status: ${payment?.status} and external_reference: ${payment?.external_reference}`);
 
-			if (info.status === 'approved') {
-				const email = info.payer.email;
-				const amount = info.transaction_amount;
+			if (payment?.status === 'approved') {
+				const email = payment.payer.email;
+				const amount = payment.transaction_amount;
 				const rifas = calcularRifas(amount);
 
 				//usuarios[info.id] = { email, rifas }; // HAY que agregar a la base.
+				// Buscar 
 
 				console.log(`✅ Pago aprobado de ${email} por $${amount}`);
 				console.log(`🎟️ Rifas asignadas: ${rifas}`);
