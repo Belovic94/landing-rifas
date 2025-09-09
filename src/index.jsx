3ee030a26d78fde920a5664310b5648b76d0c1c4
Belovic94 <victor.belosevich94@gmail.com>
2025-09-09 00:56:30 -0300

    Add email and improve css

--- src/index.jsx
+++ src/index.jsx
@@ -1,21 +1,24 @@
 import { render } from 'preact';
 import { useState, useEffect } from 'preact/hooks';
+import { TicketIcon} from '@heroicons/react/24/outline';
 
 function App() {
 	const defaultAmount = [1, 5, 10, 20];
+  const [email, setEmail] = useState('');
+  const [amount, setAmount] = useState(null);
   
 	useEffect(() => {
 
   }, []);
 
-	const handleClick = async (amount) => {
+	const handleClick = async () => {
     try {
       const response = await fetch('/create-preference', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ amount }),
+        body: JSON.stringify({amount, email}),
       });
 
       if (!response.ok) {
@@ -40,17 +43,50 @@ function App() {
 
 
   return (
-    <div class="w-full p-4 bg-gray-100">
-			<div class="flex space-x-2 mb-4">
-				{defaultAmount.map((amount) => (
+    <div className="w-full p-4 bg-gray-100">
+      <div className='w-full flex flex-col space-y-4 items-center max-w-sm mx-auto'>
+        <div class="flex space-x-4 w-full justify-center">
+          {defaultAmount.map((amount) => (
+            <label
+              key={amount}
+              className="flex items-center space-x-2 cursor-pointer flex-1"
+            >
+              <input
+                type="radio"
+                name="amount"
+                value={amount}
+                onChange={() => setAmount(amount)}
+                className="peer hidden"
+              />
+              <div className="flex items-center justify-center space-x-2 px-4 py-2 rounded-lg border-2 border-gray-300 hover:border-purple-600 transition 
+              peer-checked:border-purple-600 peer-checked:bg-purple-100 min-w-[80px]">
+                  <span className="font-medium">{amount}</span>
+                  <TicketIcon className="w-6 h-6 text-purple-600" />
+              </div>
+            </label>
+          ))}
+        </div>
+        <div className="flex items-center gap-1 w-full">
+          <label htmlFor="email" className="font-medium text-gray-700">
+            Email: 
+          </label>
+          <input
+            type="email"
+            id="email"
+            placeholder="Ingresá un email..."
+            className="px-4 py-2 border-2 border-gray-300 rounded-lg focus:outline-none focus:border-purple-600 transition  w-full"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className='flex items-center justify-center w-full'>
           <button
-            key={amount}
-						onClick={() => handleClick(amount)}
-            class="px-4 py-2 bg-purple-600 text-white rounded hover:bg-purple-700 transition"
+            onClick={handleClick}
+            className=" w-full cursor-pointer mt-4 px-10 py-3 bg-purple-600 text-white font-semibold rounded-lg hover:bg-purple-700 transition"
           >
-            {amount} rifas
+            Comprar
           </button>
-        ))}
+        </div>
       </div>
     </div>
   );
